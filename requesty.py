628586f2a5be93c7bbcfd28a2ab2e258450fe416
tchartchke <chartai.kl@gmail.com>
2021-10-11 13:43:45 -0500

    removes special pages

--- requesty.py
+++ requesty.py
@@ -4,10 +4,10 @@ import requests, re, random
 
 travelled_path = []
 r = requests.get('https://en.wikipedia.org/wiki/Special:Random')
-# r = requests.get('https://en.wikipedia.org/wiki/Dudley_Narborough')
+# r = requests.get('https://en.wikipedia.org/wiki/That_Was_the_Week_That_Was')
 page_content = r.content.decode()
 
-start = re.findall(r'<h1 id="firstHeading" class="firstHeading" >(.{1,}?)<\/h1>', page_content)
+start = re.findall(r'<title>(.{1,}?) - Wikipedia', page_content)
 travelled_path.append(start[0])
 
 for i in range(20):
@@ -15,14 +15,13 @@ for i in range(20):
   select = random.randrange(0, len(links))
   to_click = links[select][1]
 
-  while to_click in travelled_path:
+  while to_click in travelled_path or ":" in to_click:
     print(" Already clicked, selecting new link.")
     select = random.randrange(0, len(links))
     to_click = links[select][1]
 
   travelled_path.append(to_click)
   r = requests.get('https://en.wikipedia.org/wiki/%s' % links[select][0])
-
 
 print(travelled_path)
 
