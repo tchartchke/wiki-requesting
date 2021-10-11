008d667aeefb095ba4a46fc143a9080b9cf64d77
tchartchke <chartai.kl@gmail.com>
2021-10-11 12:13:32 -0500

    prevent revisiting links

--- requesty.py
+++ requesty.py
@@ -13,8 +13,14 @@ travelled_path.append(start[0])
 for i in range(20):
   links = re.findall(r'<a href="\/wiki\/([^"]{1,}?)" title="([^"]{1,}?)">', page_content)
   select = random.randrange(0, len(links))
+  to_click = links[select][1]
 
-  travelled_path.append(links[select][1])
+  while to_click in travelled_path:
+    print(" Already clicked, selecting new link.")
+    select = random.randrange(0, len(links))
+    to_click = links[select][1]
+
+  travelled_path.append(to_click)
   r = requests.get('https://en.wikipedia.org/wiki/%s' % links[select][0])
 
 
